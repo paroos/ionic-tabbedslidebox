fbf873a3659bcb21ba6b17c369ea2e1e496eefad
joaolpinho@gmail.com <joaolpinho@gmail.com>
2016-03-01 23:00:06 +0000

    <feat> gulp clean & .min ext on min

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,57 @@
 /**
  * Created by joao.pinho on 01/03/2016.
  */
-var gulp = require('gulp'),
-    path = require('path' ),
-    cssmin = require('gulp-uglifycss'),
-    jsmin = require('gulp-uglify');
+var gulp = require( 'gulp' ),
+    path = require( 'path' ),
+    rimraf = require( 'rimraf' ),
+    rename = require( 'gulp-rename' ),
+    cssmin = require( 'gulp-uglifycss' ),
+    jsmin = require( 'gulp-uglify' );
 
 
 var paths = {
-    css: {
-        src: path.join(__dirname, 'src', '**', '*.css'),
-        dest: path.join(__dirname, 'dist')
-    },
-    js: {
-        src: path.join(__dirname, 'src', '**', '*.js'),
-        dest: path.join(__dirname, 'dist')
-    }
-}
+    dist:path.join( __dirname, 'dist'),
+    src: path.join( __dirname, 'src'),
+};
 
-gulp.task('min:css', function() {
-    return gulp.src(paths.css.src)
-        .pipe(cssmin())
-        .pipe(gulp.dest(paths.css.dest));
-});
-
-gulp.task('min:js', function() {
-    return gulp.src(paths.js.src)
-        .pipe(jsmin())
-        .pipe(gulp.dest(paths.js.dest));
-});
-gulp.task('min', ['min:css', 'min:js']);
+paths.css = {
+    src: path.join( paths.src, '**', '*.css' ),
+    dest: path.join( paths.dist )
+};
+paths.js = {
+    src: path.join( paths.src, '**', '*.js' ),
+    dest: path.join( paths.dist )
+};
 
 
-gulp.task('default', ['min']);
+
+gulp.task( 'min:css', function (  ) {
+    return gulp.src( paths.css.src )
+        .pipe( cssmin() )
+        .pipe( rename( {
+            suffix: '.min'
+        } ) )
+        .pipe( gulp.dest( paths.css.dest ) )
+} );
+
+gulp.task( 'min:js', function (  ) {
+    return gulp.src( paths.js.src )
+        .pipe( jsmin() )
+        .pipe( rename( {
+            suffix: '.min'
+        } ) )
+        .pipe( gulp.dest( paths.js.dest ) );
+} );
+
+gulp.task( 'min', [  'min:css', 'min:js' ], function( done ) {
+    done();
+} );
+
+
+gulp.task( 'clean', function ( done ) {
+    rimraf( paths.dist, done );
+} );
+
+
+gulp.task( 'build', [ 'min' ] );
+gulp.task( 'default', [ 'build' ] );
